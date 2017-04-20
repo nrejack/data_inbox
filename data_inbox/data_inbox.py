9c4c4fa9cbcaa36245f2619ff7b9a9fdaf3bb006
Nicholas Rejack <nrejack@ufl.edu>
2017-04-20 15:09:18 -0400

    Adds shell of main program with logging.

---
+++ data_inbox/data_inbox.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+"""
+data_inbox: data quality checker
+"""
+
+import logging
+import sqlite3
+import shutil
+import click
+
+FILESET_DATABASE = 'fileset_db.sqlite'
+FILESET_DATABASE_BACKUP = 'fileset_db.sqlite.bk'
+
+@click.option('-v', '--verbose', help='Run in verbose mode.', is_flag=True, default=False)
+@click.command()
+def main(verbose):
+    # set up logging
+    logger = logging.getLogger()
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    logger.setLevel(logging.DEBUG)
+    # logging to file
+    fh = logging.FileHandler('data_inbox.log')
+    fh.setLevel(logging.DEBUG)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    # logging to console
+    ch = logging.StreamHandler()
+    if verbose:
+        ch.setLevel(logging.DEBUG)
+    else:
+        ch.setLevel(logging.WARNING)
+    ch.setFormatter(formatter)
+    logger.addHandler(ch)
+
+    logger.info("Starting data_inbox.py")
+    # open database
+    logger.info("Backing up database {} to {}".format(FILESET_DATABASE, FILESET_DATABASE_BACKUP))
+    shutil.copy2(FILESET_DATABASE, FILESET_DATABASE_BACKUP)
+    logger.info("Opening database: {}".format(FILESET_DATABASE))
+    conn = sqlite3.connect(FILESET_DATABASE)
+    c = conn.cursor()
+
+    logger.info("Committing changes to {}".format(FILESET_DATABASE))
+    conn.commit()
+    logger.info("Closing {}".format(FILESET_DATABASE))
+    conn.close()
+
+
+if __name__ == '__main__':
+    main()
