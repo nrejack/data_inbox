0c9c1cd021b7fbfa4ae033a1db7fe3019e978a72
Nicholas Rejack <nrejack@ufl.edu>
2017-05-02 11:39:29 -0400

    Adds SQL file with list of error codes.

---
+++ error_codes.sql
@@ -0,0 +1,8 @@
+--error codes data file
+INSERT INTO error_codes (id, error)
+VALUES
+  (0, 'No change'),
+  (1, 'New column'),
+  (2, 'Column deleted'),
+  (3, 'Header missing'),
+  (4, 'New filename pattern');
