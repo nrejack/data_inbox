aa7199bc518a0a6270c6d1ef2545caef90fcfcd3
Nicholas Rejack <nrejack@ufl.edu>
2018-05-31 15:59:47 -0400

    feat: add filetypes for CDM 4.0 to SQL files.

--- sql/filetypes.sql
+++ sql/filetypes.sql
@@ -33,4 +33,10 @@ VALUES
   (31, 'HCN_VitalFeed'),
   (32, 'Unknown'),
   (33, 'Tumor'),
-  (34, 'Tumor_Registry_Concept') --ORH-specific
+  (34, 'Tumor_Registry_Concept'),  --ORH-specific
+  (35, 'Provider'), --CDM 4.0
+  (36, 'Obs_gen'), --CDM 4.0
+  (37, 'Obs_clin'), --CDM 4.0
+  (38, 'Med_admin'), --CDM 4.0
+  (39, 'PCORNET_TRIAL'),
+  (40, 'PRO_CM')
---
+++ sql/upgrade/upgrade_40.sql
@@ -0,0 +1,9 @@
+--filetypes data file
+INSERT INTO filetypes (filetype_id, filetype_name)
+VALUES
+(35, 'Provider'), --CDM 4.0
+(36, 'Obs_gen'), --CDM 4.0
+(37, 'Obs_clin'), --CDM 4.0
+(38, 'Med_admin'), --CDM 4.0
+(39, 'PCORNET_TRIAL'),
+(40, 'PRO_CM');
